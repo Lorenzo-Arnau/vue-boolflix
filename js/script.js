05fadae142af33c4d308c998fbb793244370f900
Lorenzo-Arnau <skyline03@hotmail.it>
2021-02-10 11:56:03 +0100

    all milestones done and works
    starting work on bonus

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
+const votazione = 'vote_average';
 new Vue({
   el: '#root',
   data: {
       listMovies:[],
+      listTvSeries:[],
+      allList:[],
       userSearch:'',
+      listLang: ['en','de','fr','it','es']
   },
   mounted() {
 
@@ -14,16 +18,48 @@ new Vue({
         .then(function(response) {
          self.listMovies = response.data.results;
          console.log( self.listMovies);
+         self.makingRating(self.listMovies,votazione);
+         });
+    },
+    searchAPItvSeries : function(){
+      const self = this;
+        axios.get('https://api.themoviedb.org/3/search/tv?api_key=7f4bd6b9c8030a418c2d09489d3ddda7&language=it_IT&query=' + self.userSearch)
+        .then(function(response) {
+         self.listTvSeries = response.data.results;
+         console.log( self.listTvSeries);
+         self.makingRating(self.listTvSeries,votazione);
+         self.allList = self.listMovies.concat(self.listTvSeries)
+         console.log(self.allList);
+         self.checkingTitle(self.allList)
         });
     },
+    checkingTitle :function(array){
+      array.forEach((item, i) => {
+        if (item.title == item.original_title) {
+          item.original_title = '';
+        }
+      });
+    },
+    makingRating :function(array,vote){
+      array.forEach((item, i) => {
+        item[vote] =   Math.floor(item[vote] / 2);
+      })
+    },
+    langToFlag : function(lang){
+      if (this.listLang.includes(lang)) {
+        return lang
+      }else {
+        return 'others'
+      }
+    },
+    imageNotAvailable : function(imgPath){
+      if (imgPath != null) {
+        return 'https://image.tmdb.org/t/p/w500/'+ imgPath;
+      }else {
+        return 'img/notAvailable.png'
+      }
+    }
+
   },
 });
 Vue.config.devtools = true
-
-
-// Axios CALL
-// const self = this;
-//   axios.get('API')
-//   .then(function(response) {
-//     response.data.response
-//   });
